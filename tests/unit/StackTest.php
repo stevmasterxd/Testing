b521aa8db98cfd2b9416af90bce7379e766888de
steven <stevenmanuel1234@gmail.com>
2024-10-09 13:26:54 -0400

    commit 1

---
+++ tests/unit/StackTest.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\Attributes\DataProvider;
+final class StackTest extends TestCase
+{
+    #[DataProvider(methodName: 'DataProvider')]
+    #[TestDox('Suma de dos numeros enteros')]
+    #[Test]
+    public function Sumar($expected, int $a, int $b): void
+    {
+        $this->assertSame($expected, $a + $b);
+    }
+    private static function DataProvider(): array
+    {
+        return [
+            'data set 1' => [0, 0, 0],
+            'data set 2' => [1, 0, 1],
+            'data set 3' => [1, 1, 0],
+            'data set 4' => [3, 1, 1]
+
+        ];
+    }
+}
