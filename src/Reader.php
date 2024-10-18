0845982c5e594cd986ebe9a4f3f0731d887b97dd
steven <stevenmanuel1234@gmail.com>
2024-10-18 17:45:57 -0400

    commit 11

--- src/Reader.php
+++ src/Reader.php
@@ -2,7 +2,6 @@
 declare(strict_types=1);
 
 namespace Stev\Testing;
-use Ramsey\Uuid\Uuid;
 class Reader
 {
     private $nameReader;
--- tests/unit/ReaderTest.php
+++ tests/unit/ReaderTest.php
@@ -19,6 +19,28 @@ final class ReaderTest extends TestCase
         $this->assertSame([$book], $reader->getListOfBorrowedBooks());
         $this->assertEquals('borrowed', $book->getState());
     }
+    /**
+     * @test
+     */
+    public function readerReturnBookTest(): void
+    {
+        $id = Uuid::uuid4()->toString();
+        $reader = new Reader('Jose', $id, []);
+        $id2 =  Uuid::uuid4()->toString();
+        $book = new Book('Don Quijote de la Mancha','Miguel De Cervantes', $id2, Book::AVAILABLE);
+        $id2 = Uuid::uuid4()->toString();
+        $book2 = new Book('Juego de Tronos','GORGE R R Martin',$id2,Book::BORROWED);
+        $reader->bookLoan($book);
+        $this->assertEquals('borrowed',$book->getState());
+        $reader->returnBook($book);
+        $this->assertEquals('available',$book->getState());
+
+    }
+    public function readerShowBooksTest(): void
+    {
+
+    }
+
 
 
 }
